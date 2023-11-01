43053ace6f6d9d9b5a7b7ae625150fdf5f9fbd0c
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2023-11-01 10:40:54 +0000

    Update main.tf

--- Terraform/Base_Examples/Users/main.tf
+++ Terraform/Base_Examples/Users/main.tf
@@ -1,1 +1,48 @@
-tbc
+terraform {
+  required_providers {
+    restapi = {
+      source = "Mastercard/restapi"
+      version = "1.18.2"
+    }
+    onelogin = {
+      source  = "onelogin/onelogin"
+      version = "0.2.0"
+    }
+  }
+}
+
+provider "restapi" {
+  # Configuration options
+   uri                  = "https://api.us.onelogin.com/"
+   write_returns_object = true
+   oauth_client_credentials {
+      oauth_client_id = var.client_id
+      oauth_client_secret = var.client_secret
+      oauth_token_endpoint = "https://api.us.onelogin.com/auth/oauth2/v2/token"
+  }
+}
+
+provider "onelogin" {
+  # Configuration options
+client_id = var.client_id
+client_secret = var.client_secret
+}
+
+#### DA Users via OneLogin Provider
+# create a new user using the offical OneLogin Terraform Provider
+resource onelogin_users username {
+  username = var.user1_username
+  email    = var.user1_email
+  firstname = var.user1_firstname
+  lastname = var.user1_lastname
+  status = 1
+  state = 1
+  custom_attributes = {food = "pizza"}
+}
+
+#### DA Users via generic REST Provider
+## example of how to create a second new user in your OneLogin environment this time using the generic Rest Provider and our V2 users endpoint
+resource "restapi_object" "oneloginuser1" {
+  path = "/api/2/users"
+  data = "{ \"email\": \"${var.user2_email}\", \"username\": \"${var.user2_username}\", \"firstname\": \"${var.user2_firstname}\", \"lastname\": \"${var.user2_lastname}\"}"
+}
