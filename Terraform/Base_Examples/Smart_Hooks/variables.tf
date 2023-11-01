906ac71edb3f40d242ad63d9c229faf08156da32
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2023-11-01 15:19:08 +0000

    Update variables.tf

--- Terraform/Base_Examples/Smart_Hooks/variables.tf
+++ Terraform/Base_Examples/Smart_Hooks/variables.tf
@@ -1,1 +1,26 @@
-tbc
+variable "ol_subdomain" {
+  type = string
+  description = "Subdomain name for target OneLogin env"
+}
+
+variable "ol_client_id" {
+  type = string
+  description = "Client ID for API Credential created in target OneLogin env"
+}
+
+variable "ol_client_secret" {
+  type = string
+  description = "Client Secret for API Credential created in target OneLogin env"
+}
+
+variable "ol_smart_hook_env_var1" {
+  type = string
+  description = "Name of the Smart Hooks Env Var for the New user first time login policy- used in pre-auth smart hook"
+  default = "test"
+}
+
+variable "ol_policy_id_new_user" {
+  type = string
+  description = "User Security Policy ID for New user first time login- used in pre-auth smart hook"
+  default = "1234"
+}
