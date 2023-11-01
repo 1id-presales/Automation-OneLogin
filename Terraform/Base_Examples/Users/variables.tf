6d9f42d8f5c12f8de67557bbdfdef5cf1134e10b
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2023-11-01 10:43:36 +0000

    Create variables.tf

---
+++ Terraform/Base_Examples/Users/variables.tf
@@ -0,0 +1,51 @@
+variable "client_id" {
+  type = string
+  default = ""
+}
+
+variable "client_secret" {
+  type = string
+  default = ""
+}
+
+########### Users Vars ###########
+
+variable "user1_username" {
+  type = string
+  default = ""
+}
+
+variable "user1_email" {
+  type = string
+  default = ""
+}
+
+variable "user1_firstname" {
+  type = string
+  default = ""
+}
+
+variable "user1_lastname" {
+  type = string
+  default = ""
+}
+
+variable "user2_username" {
+  type = string
+  default = ""
+}
+
+variable "user2_email" {
+  type = string
+  default = ""
+}
+
+variable "user2_firstname" {
+  type = string
+  default = ""
+}
+
+variable "user2_lastname" {
+  type = string
+  default = ""
+}
