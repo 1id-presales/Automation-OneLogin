ad182a0301f4f547d5699ab2b51cd6564145699d
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2023-11-01 15:18:53 +0000

    Update main.tf

--- Terraform/Base_Examples/Smart_Hooks/main.tf
+++ Terraform/Base_Examples/Smart_Hooks/main.tf
@@ -4,10 +4,6 @@ terraform {
       source = "Mastercard/restapi"
       version = "1.18.2"
     }
-    onelogin = {
-      source  = "onelogin/onelogin"
-      version = "0.2.0"
-    }
   }
 }
 
@@ -22,8 +18,39 @@ provider "restapi" {
   }
 }
 
-provider "onelogin" {
-  # Configuration options
-client_id = var.ol_client_id
-client_secret = var.ol_client_secret
+############ Var for Smart Hook function ################
+
+variable "ol_smart_hook_function" {
+  type = string
+  description = "function for the pre-auth smart hook"
+  default = <<EOF
+    exports.handler = async (context) => {
+const NewUserPol_ID = process.env.NewUserPol;
+console.log("Context: ", context);
+
+  return {
+    success: true,
+    user: {
+      policy_id: context.user.policy_id
+    }
+  }
+}
+    EOF
+}
+
+############ Smart Hook env vars ################
+
+## example of how to create some env vars for Smart Hooks to use
+resource "restapi_object" "oneloginsmarthook_vars" {
+  path = "/api/2/hooks/envs"
+  data = "{ \"name\": \"${var.ol_smart_hook_env_var1}\", \"value\": \"${var.ol_policy_id_new_user}\"}"
+}
+
+############ Smart Hook ################
+
+## example of how to create a new pre auth smarthook in your OneLogin environment
+resource "restapi_object" "oneloginsmarthook_pa" {
+  path = "/api/2/hooks"
+  depends_on = [restapi_object.oneloginsmarthook_vars]
+  data = "{ \"type\": \"pre-authentication\", \"disabled\":false, \"runtime\":\"nodejs18.x\", \"context_version\":\"1.1.0\", \"retries\":0, \"timeout\":1, \"options\":{\"location_enabled\":true, \"risk_enabled\":true, \"mfa_device_info_enabled\":true}, \"env_vars\":[\"${var.ol_smart_hook_env_var1}\"], \"packages\": {\"axios\": \"0.21.1\"} , \"function\":\"${base64encode(var.ol_smart_hook_function)}\"}"
 }
