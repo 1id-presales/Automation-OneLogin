434707500d28291a27f512b3cbc734b11d197793
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2023-11-03 11:10:48 +0000

    Update variables.tf

--- Terraform/Base_Examples/Applications/variables.tf
+++ Terraform/Base_Examples/Applications/variables.tf
@@ -1,1 +1,16 @@
-tbc
+variable "ol_subdomain" {
+  type = string
+  description = "Subdomain name for target OneLogin env"
+}
+
+variable "ol_client_id" {
+  type = string
+  description = "Client ID for API Credential created in target OneLogin env"
+}
+
+variable "ol_client_secret" {
+  type = string
+  description = "Client Secret for API Credential created in target OneLogin env"
+}
+
+########### Apps Vars ###########
