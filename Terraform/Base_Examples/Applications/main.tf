240c636b5cc26ab802ac89b26df4e17736fc587d
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2023-11-03 13:23:13 +0000

    Update main.tf

--- Terraform/Base_Examples/Applications/main.tf
+++ Terraform/Base_Examples/Applications/main.tf
@@ -36,7 +36,7 @@ client_secret = var.ol_client_secret
 ## example of how to create and configure a new OIDC app in your OneLogin environment using the generic OIDC connector
 resource "restapi_object" "oidcapp1" {
   path = "/api/2/apps"
-  data = "{ \"connector_id\": \"108419\", \"brand_id\": \"${var.oidc_app1_brand_id}\", \"name\": \"${var.oidc_app1_name}\", \"visible\" : \"${var.oidc_app1_visibility}\" , \"notes\" : \"${var.oidc_app1_notes}\", \"description\":\"${var.oidc_app1_description}\", \"configuration\":{\"redirect_uri\": \"${var.oidc_app1_redirect_uri}\" , \"post_logout_redirect_uri\": \"${var.oidc_app1_logout_redirect_uri}\", \"login_url\":\"${var.oidc_app1_login_url}\", \"oidc_application_type\":\"${var.oidc_app1_oidc_application_type}\", \"token_endpoint_auth_method\":\"${var.oidc_app1_token_endpoint_auth_method}\"}}"
+  data = "{ \"connector_id\": \"108419\", \"brand_id\": \"${var.oidc_app1_brand_id}\", \"name\": \"${var.oidc_app1_name}\", \"visible\" : \"${var.oidc_app1_visibility}\" , \"notes\" : \"${var.oidc_app1_notes}\", \"description\":\"${var.oidc_app1_description}\", \"configuration\":{\"redirect_uri\": \"${var.oidc_app1_redirect_uri}\" , \"post_logout_redirect_uri\": \"${var.oidc_app1_logout_redirect_uri}\", \"login_url\":\"${var.oidc_app1_login_url}\", \"oidc_application_type\":\"${var.oidc_app1_oidc_application_type}\", \"token_endpoint_auth_method\":\"${var.oidc_app1_token_endpoint_auth_method}\",\"access_token_expiration_minutes\":\"${var.oidc_app1_access_token_exp_mins}\", \"refresh_token_expiration_minutes\":\"${var.oidc_app1_refresh_token_exp_mins}\"}}"
 }
 
 ### Generic SAML Advanced Connector APP ###
