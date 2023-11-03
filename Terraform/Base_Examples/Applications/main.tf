c3b164e652c9756428c70e0ac17afb692da21572
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2023-11-03 11:11:39 +0000

    Update main.tf

--- Terraform/Base_Examples/Applications/main.tf
+++ Terraform/Base_Examples/Applications/main.tf
@@ -1,1 +1,29 @@
-tbc
+terraform {
+  required_providers {
+    restapi = {
+      source = "Mastercard/restapi"
+      version = "1.18.2"
+    }
+    onelogin = {
+      source  = "onelogin/onelogin"
+      version = "0.2.0"
+    }
+  }
+}
+
+provider "restapi" {
+  # Configuration options
+   uri                  = "https://${var.ol_subdomain}.onelogin.com/"
+   write_returns_object = true
+   oauth_client_credentials {
+      oauth_client_id = var.ol_client_id
+      oauth_client_secret = var.ol_client_secret
+      oauth_token_endpoint = "https://${var.ol_subdomain}.onelogin.com/auth/oauth2/v2/token"
+  }
+}
+
+provider "onelogin" {
+  # Configuration options
+client_id = var.ol_client_id
+client_secret = var.ol_client_secret
+}
