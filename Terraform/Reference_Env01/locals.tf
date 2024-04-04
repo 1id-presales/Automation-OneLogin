ebe8c0a66cc53e308b87de6c3649179a6c6cf617
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2024-04-04 08:27:05 +0100

    Create locals.tf

---
+++ Terraform/Reference_Env01/locals.tf
@@ -0,0 +1,50 @@
+locals {
+  ol_users = [
+    {
+      username   = "James.smith"
+      firstname  = "James"
+      lastname   = "smith"
+      email      = "james.smith@Cedarstone.com"
+      department = "Sales ops"
+      title      = "Manager"
+      company    = "Cedarstone"
+      phone      = "1234445566"
+      state      = 1
+      status     = 1
+      custom_attributes = {
+        staff_id = "12345"
+        personal_mail = "james.smith66666666@gmail.com"
+      }
+    },
+    # Add more standard users here
+  ]
+ol_application_object = [
+  {
+            connector_id : 68332,
+            name : "Wrike"
+    description = "BU: Sales operations"
+    notes = "custom_attribute_app_Wrike"
+    visible = true
+    allow_assumed_signin = false
+    birthright_app = ""
+    app_owner = "james.smith@Cedarstone.com"      # required -  email address of admin user responsible for configuring up the app connector
+        }
+# Add more Apps here
+]
+
+ol_application_with_app_policy_object = [
+{
+            connector_id : 68251,
+            name : "Lever"
+            policy_id = var.ol_high_risk_app_policy_id
+    tab_id = var.ol_custom_tab1_id
+    description = "BU: Sales operations"
+    notes = "custom_attribute_app_Lever"
+    visible = true
+    allow_assumed_signin = false
+    birthright_app = ""
+    app_owner = ""      # required -  email address of admin user responsible for configuring up the app connector
+        },
+# Add more Apps here
+]
+}
